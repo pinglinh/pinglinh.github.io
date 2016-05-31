8b2ba7ef11da026508ac44324685105c256f1e9d
Linh Nguyen My <lnguyenmy1@gmail.com>
2016-05-31 23:29:05 +0100

    Finally the nav bar works

--- javascript.js
+++ javascript.js
@@ -9,34 +9,34 @@ function hasClass(elem, className) {
 }
 // addClass
 function addClass(elem, className) {
-  if (!hasClass(elem, className)) {
-    elem.className += ' ' + className;
-  }
+    if (!hasClass(elem, className)) {
+      elem.className += ' ' + className;
+    }
 }
 // removeClass
 function removeClass(elem, className) {
-  var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, ' ') + ' ';
+  var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
   if (hasClass(elem, className)) {
-    while (newClass.indexOf(' ' + className + ' ') >= 0) {
-      newClass = newClass.replace(' ' + className + ' ', ' ');
+        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+            newClass = newClass.replace(' ' + className + ' ', ' ');
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
     }
-    elem.className = newClass.replace(/^\s+|\s+$/g, '');
-  }
 }
 // toggleClass
 function toggleClass(elem, className) {
-  var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, " ") + ' ';
-  if (hasClass(elem, className)) {
-    while (newClass.indexOf(" " + className + " ") >= 0) {
-      newClass = newClass.replace(" " + className + " ", " ");
+  var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(" " + className + " ") >= 0 ) {
+            newClass = newClass.replace( " " + className + " " , " " );
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    } else {
+        elem.className += ' ' + className;
     }
-    elem.className = newClass.replace(/^\s+|\s+$/g, '');
-  } else {
-    elem.className += ' ' + className;
-  }
 }
 
 theToggle.onclick = function() {
-  toggleClass(this, 'on');
-  return false;
+   toggleClass(this, 'on');
+   return false;
 }
